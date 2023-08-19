49b6fe2c2199e27aef326efccffc8a0b0d5e8f19
AndresB1013 <giovanny_ballen@hotmail.com>
2023-08-19 08:25:59 -0500

    Se añadio la propiedad TItle en iframe del mapa

--- src/components/Mapa.jsx
+++ src/components/Mapa.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 
 function Mapa(props) {
     return (
-        <iframe src={props.src}
-        width="500"
-        height="350"
-        style={{border:-1}}
-        allowFullscreen=""
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"></iframe>
+        <iframe
+            title='Ubicaciones'
+            src={props.src}
+            width={500} height={350}
+            style={{ border: '-1' }}
+            allowFullScreen loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade">
+        </iframe>
+
     );
 }
 
