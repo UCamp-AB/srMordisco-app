178192ac356fc6c090874f401fc2053af493fb16
AndresB1013 <giovanny_ballen@hotmail.com>
2023-08-19 07:57:15 -0500

    Media query para el Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,19 @@ import logo from "../assets/Logo.jpg";
 import '../App.css';
 import { Link } from "react-router-dom";
 
+
 function Header() {
     return (
-    <header className="header-container">
-        <Link to={'/'} ><img src={logo} alt="Logo" /></Link>
-        <h1>Sr. Mordisco</h1>
-        <ul className="nav nav-pills nav-underline">
-            <li className="nav-item"><Link className="nav-link text-decoration-none" to={'/inicio'}>Inicio</Link></li>
-            <li className="nav-item"><Link className="nav-link text-decoration-none" to={'/nosotros'}>Acerca de Nosotros</Link></li>
-            <li className="nav-item"><Link className="nav-link text-decoration-none" to={'/Menu'}>Menú</Link></li>
-        </ul>
-    </header>
+        <header className="header-container">
+
+            <Link to={'/'} ><img src={logo} alt="Logo" /></Link>
+            <h1>Sr. Mordisco</h1>
+            <ul className="nav nav-pills nav-underline">
+                <li className="nav-item"><Link className="nav-link text-decoration-none" to={'/inicio'}>Inicio</Link></li>
+                <li className="nav-item"><Link className="nav-link text-decoration-none" to={'/nosotros'}>Acerca de Nosotros</Link></li>
+                <li className="nav-item"><Link className="nav-link text-decoration-none" to={'/Menu'}>Menú</Link></li>
+            </ul>
+        </header>
     );
 }
 
