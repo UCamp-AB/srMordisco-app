a4c703b963012e849536b48b19ae7e2c2469d8b8
AndresB1013 <giovanny_ballen@hotmail.com>
2023-10-24 17:32:55 -0500

    Agregar etiqueta de monitorización de Google analytics

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,17 +3,17 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 //Mis páginas
 import Home from '../pages/Home';
 import AcercaDe from '../pages/AcercaDe';
-import Reserva from '../pages/Reserva';
 import Menu from '../pages/Menu';
+import Reserva from '../pages/Reserva';
 
 function AppRouter() {
     return (
         <Routes>
-            <Route path="/" element={<Navigate to="/inicio"/>}/>
-            <Route path="/reserva/:sede" element={<Reserva/>} />
             <Route path="inicio" element={<Home/>}/>
             <Route path="nosotros" element={<AcercaDe/>}/>
             <Route path="menu" element={<Menu/>}/>
+            <Route path="/reserva/:sede" element={<Reserva/>} />
+            <Route path="/" element={<Navigate to="/inicio"/>}/>
         </Routes>
     );
 }
